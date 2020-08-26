1324da2f6b07c5cd2f5b523ebed72b2cf4ab18a9
Thorsten Mueller <MrThorstenM@gmx.net>
2020-08-26 19:55:39 +0200

    Added scripts for webhook

--- MyCroft/module.php
+++ MyCroft/module.php
@@ -1,56 +1,94 @@
 <?php
 
-    include_once __DIR__ . '/registry.php';
-
     // Klassendefinition
     class mycroft extends IPSModule {
         
         private $registry = null;
-
-        public function __construct($InstanceID)
-        {
-            parent::__construct($InstanceID, 'mycroft');
-    
-            $this->registry = new DeviceTypeRegistry(
-                $this->InstanceID,
-                function ($Name, $Value)
-                {
-                    $this->RegisterPropertyString($Name, $Value);
-                }
-            );
-        }
+        private static $supportedDeviceTypes = array(
+            'DeviceLightSwitch'
+            ,'DeviceLightDimmer'
+            ,'DeviceColorController'
+            ,'DeviceLock'
+            ,'DeviceTemperatureSensor'
+            ,'DeviceThermostat'
+            ,'DeviceSpeaker'
+            ,'DeviceSpeakerMuteable'
+            ,'DeviceShutter'
+            ,'DeviceGenericSwitch'
+            ,'DeviceGenericSlider'
+        );
 
         // Überschreibt die interne IPS_Create($id) Funktion
         public function Create() {
             // Diese Zeile nicht löschen.
             parent::Create();
 
-            $this->registry->registerProperties();
-
- 
+            //$this->registry->registerProperties();
+            //Add all deviceType specific properties
+            foreach (self::$supportedDeviceTypes as $actionType) {
+                $this->registerPropertyString($actionType, '[]');
+            }
         }
  
+        public function updateProperties(): void
+        {
+            $ids = [];
+            
+            //Check that all IDs have distinct values and build an id array
+            foreach (self::$supportedDeviceTypes as $actionType) {
+                $datas = json_decode(IPS_GetProperty($this->InstanceID, $actionType), true);
+                foreach ($datas as $data) {
+                    //Skip over uninitialized zero values
+                    if ($data['ID'] != '') {
+                        //$this->SendDebug("bla",$data['ID'],0);
+                        if (in_array($data['ID'], $ids)) {
+                            throw new Exception('ID has to be unique for all devices');
+                        }
+                        $ids[] = $data['ID'];
+                    }
+                }
+            }
+        }
+
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() {
             // Diese Zeile nicht löschen
             parent::ApplyChanges();
+            $this->updateProperties();
 
-            $this->registry->updateProperties();
+            $this->createScriptGetItems();
+            $this->createScriptGetValue();
 
-            $objectIDs = $this->registry->getObjectIDs();
-
-            if (method_exists($this, 'GetReferenceList')) {
-                $refs = $this->GetReferenceList();
-                foreach ($refs as $ref) {
-                    $this->UnregisterReference($ref);
-                }
-
-                foreach ($objectIDs as $id) {
-                    $this->RegisterReference($id);
-                }
-            }
         }
         
+        private function createScriptGetItems() {
+            $scriptTemplate = '
+                <?php
+
+                    $deviceType = $_GET["deviceType"];
+                    $json = IPS_GetProperty(' . $this->InstanceID . ',"Device" . $deviceType);
+                    echo($json);
+
+                ?>
+            
+            ';
+
+            $scriptId = $this->RegisterScript("mycroft_get_items","MyCroft get devices by categorie",$scriptTemplate);
+        }
+
+        private function createScriptGetValue() {
+            $scriptTemplate = '
+                <?php
+
+                    $objectId = $_GET["objectId"];
+                    echo(GetValue($objectId));
+
+                ?>
+            
+            ';
+
+            $scriptId = $this->RegisterScript("mycroft_get_value","Read value from variable",$scriptTemplate);
+        }
  
         /**
         * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
